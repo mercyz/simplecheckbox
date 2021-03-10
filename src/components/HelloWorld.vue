4ab8d1fc81345e3bbd768a31515cd62065b33265
Uzor Mercy <mercy4hope@gmail.com>
2021-03-10 18:30:39 +0100

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+      <div class="card"  @click="checkbox($event)" v-for="(username, index) in codepally" :key="index">
+        <input class="checkbox" type="checkbox" :value="username[0]"  v-model="selected"> 
+        <label class="username" for="{username[0]}">{{username[1]}}</label>
+      </div>
+      <p class="flex">{{selected.join(",")}}</p>
   </div>
 </template>
-
 <script>
+import { codepally_data } from "../lib/app.js";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+      name: "Hello",
+      data: () => ({
+          selected: [],
+          codepally: [],
+      }),
+      mounted: function(){
+         let c = Object.keys(codepally_data).map((key) => [key, codepally_data[key]]);
+          this.codepally = c
+      },
+      methods: {
+         checkbox(event){
+            if(event.target.tagName == "LABEL"){
+              event.target.parentNode.classList.toggle("blue")
+            }
+            if(event.target.type == "checkbox"){
+              event.target.parentNode.classList.toggle("blue")
+            }
+              event.target.classList.toggle("blue")
+        },
+      }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style>
+  .container{
+      width: 300px;
+      display: flex;
+      flex-direction: column;
+  }
+  .card{
+    margin-bottom: 5px;
+    display: flex;
+    box-shadow: 0px 1px 4px #eee;
+    padding: 0px 9px;
+    width: 240px;
+    background:white;
+    cursor: pointer;
+    }
+    .username{
+      padding:10px 3px;
+      margin:0;
+    }
+    .checkbox{
+      outline: none;
+    }
+    .blue{
+      background:dodgerblue;
+      color:white
+    }
+    .flex{
+      display: flex;
+    }
+  </style>
