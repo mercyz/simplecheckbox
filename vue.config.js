93414b10aa8eae663bc12b25a2f27a297e3773e7
Uzor Mercy <mercy4hope@gmail.com>
2021-03-10 19:31:55 +0100

    updated the deployment cdode

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/codepally/'
-      : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //   ? '/simplecheckbox/'
+    //   : '/'
+    publicPath: "simplecheckbox"
   }
